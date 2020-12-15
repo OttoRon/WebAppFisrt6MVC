239dd37d11cf2a59abfbd450fb009115ca62203a
Otto Ronkainen <otto.ronkainen@student.careeria.fi>
2020-12-15 11:59:34 +0200

    Bob

---
+++ WebAppFirst (2)/WebAppFirst/WebAppFirst/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null) // Tässä tarkastetaan onko käyttäjä kirjautunut sisään
+            {
+                return RedirectToAction("login", "home");
+            }else
+            {
+                northwindEntities1 db = new northwindEntities1();
+                List<Products> model = db.Products.ToList();
+                db.Dispose();
+                return View(model);
+            }   
+        }
+
+        public ActionResult Index2()
+        {
+            northwindEntities1 db = new northwindEntities1();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+            return View(model);
+        }
+    }
+}
---
+++ WebAppFirst (2)/WebAppFirst/WebAppFirst/Controllers/ShippersController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ShippersController : Controller
+    {
+        // GET: Shippers
+        northwindEntities1 db = new northwindEntities1();
+        public ActionResult Index()
+        {
+            var shippers = db.Shippers.Include(s => s.Region);
+            return View(shippers.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null) return HttpNotFound();
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Katso https://go.microsoft.com/fwlink/?LinkId=317598
+        public ActionResult Edit([Bind(Include = "ShipperID,CompanyName,Phone,RegionID")] Shippers shipper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shipper).State = EntityState.Modified;
+                db.SaveChanges();
+                ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shipper.RegionID);
+                return RedirectToAction("Index");
+            }
+            return View(shipper);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShipperID,CompanyName,Phone,RegionID")] Shippers shipper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shippers.Add(shipper);
+                db.SaveChanges();
+                ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription");
+                return RedirectToAction("Index");
+            }
+            return View(shipper);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null) return HttpNotFound();
+            return View(shippers);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shippers shippers = db.Shippers.Find(id);
+            db.Shippers.Remove(shippers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
